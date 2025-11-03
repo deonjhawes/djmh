1f8f32a31f0bc87cb196a1970bb04f6bea67375d
deonjhawes <deonjhawes@gmail.com>
2025-11-03 14:57:04 +0200

    hii

---
+++ functions.js
@@ -0,0 +1,36 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("google"); 
+// TODO: Make the greet function return "Hello, [name]!"
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+
+
+// TODO: Create a function called add that takes two parameters
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+
+
+// TODO: Make the add function return the sum of both parameters
+function add(a, b) {
+  return a + b;
+}
+
+
+
+// TODO: Call both functions and store their results in variables
+
+const greetings = greet("Deon")
+
+const sum = add(9,5)
+
+// TODO: Print the results to the console
+
+console.log(greetings)
+console.log(sum)
